54110c405f734c46316a32be77cf87aaa7ace07d
Oussama Slimani <slimani.oussama@enis.tn>
2023-07-30 21:17:08 +0100

    same fixing

--- js/main.js
+++ js/main.js
@@ -10,8 +10,8 @@
         }, 1);
     };
     spinner();
-    
-    
+
+
     // Initiate the wowjs
     new WOW().init();
 
@@ -24,8 +24,8 @@
             $('.sticky-top').removeClass('shadow-sm').css('top', '-100px');
         }
     });
-    
-    
+
+
     // Back to top button
     $(window).scroll(function () {
         if ($(this).scrollTop() > 300) {
@@ -35,7 +35,7 @@
         }
     });
     $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
         return false;
     });
 
@@ -46,7 +46,7 @@
         time: 2000
     });
 
-    
+
     // Testimonial carousel
     $(".testimonial-carousel").owlCarousel({
         autoplay: true,
@@ -55,7 +55,7 @@
         dots: true,
         loop: true,
         nav: true,
-        navText : [
+        navText: [
             '<i class="bi bi-chevron-left"></i>',
             '<i class="bi bi-chevron-right"></i>'
         ]
@@ -63,44 +63,44 @@
 
 
 
-   
+
     $(document).ready(function () {
-      // Portfolio isotope and filter
-      var portfolioIsotope = $('.portfolio-container').isotope({
-          itemSelector: '.portfolio-item',
-          layoutMode: 'fitRows'
-      });
+        // Portfolio isotope and filter
+        var portfolioIsotope = $('.portfolio-container').isotope({
+            itemSelector: '.portfolio-item',
+            layoutMode: 'fitRows'
+        });
 
-      // Set the active filter on page load
-      var defaultFilter = '*'; // Set the default filter ('*' for "All" items)
-      var urlFilter = getParameterByName('filter'); // Get filter value from URL parameter (if exists)
-      var activeFilter = urlFilter || defaultFilter;
-      $('#portfolio-flters li[data-filter="' + activeFilter + '"]').addClass('active');
+        // Set the active filter on page load
+        var defaultFilter = '*'; // Set the default filter ('*' for "All" items)
+        var urlFilter = getParameterByName('filter'); // Get filter value from URL parameter (if exists)
+        var activeFilter = urlFilter || defaultFilter;
+        $('#portfolio-flters li[data-filter="' + activeFilter + '"]').addClass('active');
 
-      // Filter items based on the active filter
-      portfolioIsotope.isotope({ filter: activeFilter });
+        // Filter items based on the active filter
+        portfolioIsotope.isotope({ filter: activeFilter });
 
-      // Click event for the filter items
-      $('#portfolio-flters li').on('click', function () {
-          $('#portfolio-flters li').removeClass('active');
-          $(this).addClass('active');
+        // Click event for the filter items
+        $('#portfolio-flters li').on('click', function () {
+            $('#portfolio-flters li').removeClass('active');
+            $(this).addClass('active');
 
-          portfolioIsotope.isotope({ filter: $(this).data('filter') });
-      });
+            portfolioIsotope.isotope({ filter: $(this).data('filter') });
+        });
 
-      // Function to get URL parameter value
-      function getParameterByName(name, url) {
-          if (!url) url = window.location.href;
-          name = name.replace(/[\[\]]/g, '\\$&');
-          var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
-              results = regex.exec(url);
-          if (!results) return null;
-          if (!results[2]) return '';
-          return decodeURIComponent(results[2].replace(/\+/g, ' '));
-      }
-  });
-  
-  
+        // Function to get URL parameter value
+        function getParameterByName(name, url) {
+            if (!url) url = window.location.href;
+            name = name.replace(/[\[\]]/g, '\\$&');
+            var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+                results = regex.exec(url);
+            if (!results) return null;
+            if (!results[2]) return '';
+            return decodeURIComponent(results[2].replace(/\+/g, ' '));
+        }
+    });
+
+
 })(jQuery);
 
 
