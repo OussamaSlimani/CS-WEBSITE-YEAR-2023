f61c5e69b76a1821d455504e9ab7b72ed3b72725
OussamaSlimani <slimani.oussama@enis.tn>
2023-08-10 09:44:13 +0100

    seo improvment 1.0

--- js/main.js
+++ js/main.js
@@ -1,156 +1,164 @@
 (function ($) {
-    "use strict";
+  "use strict";
 
-    // Spinner
-    var spinner = function () {
-        setTimeout(function () {
-            if ($('#spinner').length > 0) {
-                $('#spinner').removeClass('show');
-            }
-        }, 1);
-    };
-    spinner();
+  // Spinner
+  var spinner = function () {
+    setTimeout(function () {
+      if ($("#spinner").length > 0) {
+        $("#spinner").removeClass("show");
+      }
+    }, 1);
+  };
+  spinner();
 
+  // Initiate the wowjs
+  new WOW().init();
 
-    // Initiate the wowjs
-    new WOW().init();
+  // Sticky Navbar
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      $(".sticky-top").addClass("shadow-sm").css("top", "0px");
+    } else {
+      $(".sticky-top").removeClass("shadow-sm").css("top", "-100px");
+    }
+  });
 
+  // Back to top button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      $(".back-to-top").fadeIn("slow");
+    } else {
+      $(".back-to-top").fadeOut("slow");
+    }
+  });
+  $(".back-to-top").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 1500, "easeInOutExpo");
+    return false;
+  });
 
-    // Sticky Navbar
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 300) {
-            $('.sticky-top').addClass('shadow-sm').css('top', '0px');
-        } else {
-            $('.sticky-top').removeClass('shadow-sm').css('top', '-100px');
-        }
-    });
+  // Facts counter
+  $('[data-toggle="counter-up"]').counterUp({
+    delay: 10,
+    time: 2000,
+  });
 
+  // Testimonial carousel
+  $(".testimonial-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1000,
+    items: 1,
+    dots: true,
+    loop: true,
+    nav: true,
+    navText: [
+      '<i class="bi bi-chevron-left"></i>',
+      '<i class="bi bi-chevron-right"></i>',
+    ],
+  });
 
-    // Back to top button
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 300) {
-            $('.back-to-top').fadeIn('slow');
-        } else {
-            $('.back-to-top').fadeOut('slow');
-        }
-    });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
-        return false;
-    });
-
-
-    // Facts counter
-    $('[data-toggle="counter-up"]').counterUp({
-        delay: 10,
-        time: 2000
-    });
-
-
-    // Testimonial carousel
-    $(".testimonial-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1000,
+  // Testimonials carousel
+  $(".testimonial-carousel2").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1000,
+    center: true,
+    margin: 24,
+    dots: true,
+    loop: true,
+    nav: true,
+    navText: [
+      '<i class="bi bi-chevron-left"></i>',
+      '<i class="bi bi-chevron-right"></i>',
+    ],
+    responsive: {
+      0: {
         items: 1,
-        dots: true,
-        loop: true,
-        nav: true,
-        navText: [
-            '<i class="bi bi-chevron-left"></i>',
-            '<i class="bi bi-chevron-right"></i>'
-        ]
+      },
+      768: {
+        items: 2,
+      },
+      992: {
+        items: 3,
+      },
+    },
+  });
+
+  // Portfolio isotope and filter
+  $(document).ready(function () {
+    var portfolioIsotope = $(".portfolio-container").isotope({
+      itemSelector: ".portfolio-item",
+      layoutMode: "fitRows",
     });
+    var defaultFilter = "*";
+    var urlFilter = getParameterByName("filter");
+    var activeFilter = urlFilter || defaultFilter;
+    $('#portfolio-flters li[data-filter="' + activeFilter + '"]').addClass(
+      "active"
+    );
+    portfolioIsotope.isotope({ filter: activeFilter });
+    $("#portfolio-flters li").on("click", function () {
+      $("#portfolio-flters li").removeClass("active");
+      $(this).addClass("active");
 
-
-
-
-    $(document).ready(function () {
-        // Portfolio isotope and filter
-        var portfolioIsotope = $('.portfolio-container').isotope({
-            itemSelector: '.portfolio-item',
-            layoutMode: 'fitRows'
-        });
-
-        // Set the active filter on page load
-        var defaultFilter = '*'; // Set the default filter ('*' for "All" items)
-        var urlFilter = getParameterByName('filter'); // Get filter value from URL parameter (if exists)
-        var activeFilter = urlFilter || defaultFilter;
-        $('#portfolio-flters li[data-filter="' + activeFilter + '"]').addClass('active');
-
-        // Filter items based on the active filter
-        portfolioIsotope.isotope({ filter: activeFilter });
-
-        // Click event for the filter items
-        $('#portfolio-flters li').on('click', function () {
-            $('#portfolio-flters li').removeClass('active');
-            $(this).addClass('active');
-
-            portfolioIsotope.isotope({ filter: $(this).data('filter') });
-        });
-
-        // Function to get URL parameter value
-        function getParameterByName(name, url) {
-            if (!url) url = window.location.href;
-            name = name.replace(/[\[\]]/g, '\\$&');
-            var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
-                results = regex.exec(url);
-            if (!results) return null;
-            if (!results[2]) return '';
-            return decodeURIComponent(results[2].replace(/\+/g, ' '));
-        }
+      portfolioIsotope.isotope({ filter: $(this).data("filter") });
     });
-
-
+    function getParameterByName(name, url) {
+      if (!url) url = window.location.href;
+      name = name.replace(/[\[\]]/g, "\\$&");
+      var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+      if (!results) return null;
+      if (!results[2]) return "";
+      return decodeURIComponent(results[2].replace(/\+/g, " "));
+    }
+  });
 })(jQuery);
-
 
 // Get Year
 document.getElementById("currentYear").textContent = new Date().getFullYear();
 
 // initializeIsotope
 function initializeIsotope() {
-    var container = document.getElementById("portfolio-container");
-    var iso = new Isotope(container, {
-        itemSelector: ".portfolio-item",
-        layoutMode: "masonry",
-        masonry: {
-            columnWidth: ".portfolio-item"
-        }
-    });
-    imagesLoaded(container, function () {
-        iso.layout();
-    });
+  var container = document.getElementById("portfolio-container");
+  var iso = new Isotope(container, {
+    itemSelector: ".portfolio-item",
+    layoutMode: "masonry",
+    masonry: {
+      columnWidth: ".portfolio-item",
+    },
+  });
+  imagesLoaded(container, function () {
+    iso.layout();
+  });
 }
 window.addEventListener("load", initializeIsotope);
 
-// Scroll 
-document.addEventListener('DOMContentLoaded', function () {
-    const navLinks = document.querySelectorAll('.navbar-nav .nav-link');
-    const sections = document.querySelectorAll('div[id]');
-    function updateActiveNavLink() {
-        const currentScrollPos = window.scrollY;
-        let currentSection;
-        for (const section of sections) {
-            const sectionTop = section.offsetTop;
-            const sectionBottom = sectionTop + section.offsetHeight;
+// Scroll
+document.addEventListener("DOMContentLoaded", function () {
+  const navLinks = document.querySelectorAll(".navbar-nav .nav-link");
+  const sections = document.querySelectorAll("div[id]");
+  function updateActiveNavLink() {
+    const currentScrollPos = window.scrollY;
+    let currentSection;
+    for (const section of sections) {
+      const sectionTop = section.offsetTop;
+      const sectionBottom = sectionTop + section.offsetHeight;
 
-            if (currentScrollPos > sectionTop && currentScrollPos < sectionBottom) {
-                currentSection = section;
-                break;
-            }
-        }
-        if (currentSection) {
-            const sectionId = currentSection.getAttribute('id');
-            for (const link of navLinks) {
-                if (link.getAttribute('href') === `#${sectionId}`) {
-                    link.classList.add('active');
-                } else {
-                    link.classList.remove('active');
-                }
-            }
-        }
+      if (currentScrollPos > sectionTop && currentScrollPos < sectionBottom) {
+        currentSection = section;
+        break;
+      }
     }
-    updateActiveNavLink();
-    window.addEventListener('scroll', updateActiveNavLink);
+    if (currentSection) {
+      const sectionId = currentSection.getAttribute("id");
+      for (const link of navLinks) {
+        if (link.getAttribute("href") === `#${sectionId}`) {
+          link.classList.add("active");
+        } else {
+          link.classList.remove("active");
+        }
+      }
+    }
+  }
+  updateActiveNavLink();
+  window.addEventListener("scroll", updateActiveNavLink);
 });
-
